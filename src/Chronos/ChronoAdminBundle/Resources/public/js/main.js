7f6d86ef4c2f89945eb48c09ae6a14d9aad824dc
Guimove <dasilva.guillaume@live.fr>
2012-05-09 00:23:58 +0200

    add datepicker + some refactor

--- src/Chronos/ChronoAdminBundle/Resources/public/js/main.js
+++ src/Chronos/ChronoAdminBundle/Resources/public/js/main.js
@@ -1,7 +1,9 @@
-$("form input.date").datepicker({
-    dateFormat: 'dd/mm/yy',
-    defaultDate:null,
-    changeYear:true,
-    yearRange: '-60:-16',
-    firstDay:1
-}).attr("readonly","readonly");
+$(document).ready(function () {
+    $("input.date").datepicker({
+        dateFormat: 'dd/mm/yy',
+        defaultDate: null,
+        changeYear: true,
+        yearRange: '-60:-16',
+        firstDay: 1
+    }).attr("readonly", "readonly")
+});
